35367998901e03c936f34a8a9564545eb69a9f7f
Nitai Charan <nitaicharan@gmail.com>
2019-12-29 20:49:22 -0300

    58. Challenge 3 Solution
    - State
    - StateFull
    - Random

--- android/app/src/main/kotlin/com/example/dicee/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dicee/MainActivity.kt
@@ -1,4 +1,4 @@
-package co.appbrewery.dicee
+package com.example.dicee
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
           backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
-    ),
-  );
+    );
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
 }
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  int number1 = 1;
+  int number2 = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      number1 = Random().nextInt(6) + 1;
+      number2 = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png',),
-              onPressed: () => print('nitai'),
+              onPressed: () => this.changeDiceFace(),
+              child: Image.asset('images/dice$number1.png'),
             ),
           ),
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: FlatButton(
+              onPressed: () => this.changeDiceFace(),
+              child: Image.asset('images/dice$number2.png'),
+            ),
           ),
         ],
       ),
